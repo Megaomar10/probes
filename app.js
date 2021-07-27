079ce72aa5098c7f1bd072eb9af0275878ae286f
Megaomar10 <ocmega10@hotmail.com>
2021-07-26 19:29:40 -0500

    subida de proyecto

---
+++ app.js
@@ -0,0 +1,21 @@
+const cajas = document.querySelectorAll('.caja');
+const par = document.querySelector('#par');
+const impar = document.querySelector('#impar');
+const clear = document.querySelector('#clear');
+
+par.addEventListener("click", () => {
+    for (var i = 0; i < cajas.length; i++) {
+        if (i % 2 == 0) {
+            cajas[i].style.background = "blue"
+        }
+    }
+})
+
+
+impar.addEventListener("click", () => {
+    for (var i = 0; i < cajas.length; i++) {
+        if (i % 2 != 0) {
+            cajas[i].style.background = "pink";
+        }
+    }
+})
